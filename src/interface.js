4dcec66b91a1a7460cf63dc7d133492f37e34cdb
RussellWind <russell.wind@gmail.com>
2019-08-22 14:46:01 +0100

    Weather API information added

--- src/interface.js
+++ src/interface.js
@@ -31,4 +31,20 @@ $( document ).ready(function() {
     $('#temperature').text(thermostat.getCurrentTemperature())
     $('#temperature').css("color", thermostat.energyUsage())
   }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#outside-temperature').text(Math.round(data.main.temp));
+    })
+  }
+
+  displayWeather('Borough of Southend-on-Sea');
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
 });
